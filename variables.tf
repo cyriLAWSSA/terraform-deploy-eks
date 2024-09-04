bbc75c520dec82877886b1e65f51ac2d34f48c28
atangcho <atangchohfm@gmail.com>
2024-09-04 20:23:31 +0100

    first eks deploy commit

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default cidr range of vpc"
+}
+variable "aws_region" {
+  default     = "us-east-1"
+  description = "default region of vpc"
+}
